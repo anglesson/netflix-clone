f6bc6d26fc708ad023a852e6891f927244e63766
Anglesson Araujo <anglesson@hotmail.com>
2021-05-13 23:00:32 -0300

    Ajustes no layout e inclusão da lista de series.

--- js/service.js
+++ js/service.js
@@ -1,20 +1,38 @@
-axios.get('https://api.themoviedb.org/3/list/7095569?api_key=df1603189d55008780a7208650c8359d')
-  .then(function (response) {
-    // handle success
-    var owl = $('.owl-carousel');
-    response.data.items.forEach((item) => {
-      const templateItem = `<div class="item"><img class="box-filme" src="http://image.tmdb.org/t/p/w342${item.poster_path}" alt="" /></div>`;
+const apiKey = "df1603189d55008780a7208650c8359d"
+axios
+	.get(`https://api.themoviedb.org/3/list/7095569?api_key=${apiKey}`)
+	.then(function (response) {
+		// handle success
+		var owl = $("#lista-filmes");
+		response.data.items.forEach((item) => {
+			const templateItem = `<div class="item"><img class="box-filme" src="http://image.tmdb.org/t/p/w342${item.poster_path}" alt="" /></div>`;
 
-      // Listen to owl events:
-      owl.trigger('add.owl.carousel', [templateItem])
-      owl.trigger('refresh.owl.carousel', ['add'])
-    })
-    owl.owlCarousel();
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
+			// Listen to owl events:
+			owl.trigger("add.owl.carousel", [templateItem]);
+			owl.trigger("refresh.owl.carousel", ["add"]);
+		});
+		owl.owlCarousel();
+	})
+	.catch(function (error) {
+		// handle error
+		console.log(error);
+	});
+
+  axios
+	.get(`https://api.themoviedb.org/3/list/7095571?api_key=${apiKey}`)
+	.then(function (response) {
+		// handle success
+		var owl = $("#lista-series");
+		response.data.items.forEach((item) => {
+			const templateItem = `<div class="item"><img class="box-filme" src="http://image.tmdb.org/t/p/w342${item.poster_path}" alt="" /></div>`;
+
+			// Listen to owl events:
+			owl.trigger("add.owl.carousel", [templateItem]);
+			owl.trigger("refresh.owl.carousel", ["add"]);
+		});
+		owl.owlCarousel();
+	})
+	.catch(function (error) {
+		// handle error
+		console.log(error);
+	});
