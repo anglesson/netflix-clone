00a0741c625996a9e990aeda9749090ff9502b27
Anglesson Araujo <anglesson@hotmail.com>
2021-05-13 22:19:49 -0300

    Correção do refresh da lista de filmes

--- js/service.js
+++ js/service.js
@@ -1,14 +1,15 @@
 axios.get('https://api.themoviedb.org/3/list/7095569?api_key=df1603189d55008780a7208650c8359d')
   .then(function (response) {
     // handle success
+    var owl = $('.owl-carousel');
     response.data.items.forEach((item) => {
       const templateItem = `<div class="item"><img class="box-filme" src="http://image.tmdb.org/t/p/w342${item.poster_path}" alt="" /></div>`;
 
-      var owl = $('.owl-carousel');
-      owl.owlCarousel();
       // Listen to owl events:
       owl.trigger('add.owl.carousel', [templateItem])
+      owl.trigger('refresh.owl.carousel', ['add'])
     })
+    owl.owlCarousel();
   })
   .catch(function (error) {
     // handle error
